2dd80ab5143b69ff910728becde3d75873cc8611
André <AndreR@ppedv.de>
2021-07-12 11:14:12 +0200

    Simple unsafe Singelton

---
+++ HalloSingelton/HalloSingelton/Logger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HalloSingelton
+{
+    class Logger
+    {
+        static Logger _instance = null;
+
+        public static Logger Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Logger();
+
+                return _instance;
+            }
+        }
+
+        static int instanceCounter = 0;
+        private Logger()
+        {
+            instanceCounter++;
+        }
+
+        public void Log(string msg)
+        {
+            Console.WriteLine($"({instanceCounter}) [{DateTime.Now}] {msg}");
+        }
+    }
+}
---
+++ HalloSingelton/HalloSingelton/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HalloSingelton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Logger.Instance.Log("App startet");
+
+            Logger.Instance.Log("App Ende");
+
+            Console.WriteLine("Ende");
+            Console.ReadKey();
+        }
+    }
+}
