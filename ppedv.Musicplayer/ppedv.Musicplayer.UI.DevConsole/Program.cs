e871b48b268e87a36a51db96746cdadb68547eec
André <AndreR@ppedv.de>
2021-07-13 16:57:49 +0200

    Dependency Injection per Refection

--- ppedv.Musicplayer/ppedv.Musicplayer.UI.DevConsole/Program.cs
+++ ppedv.Musicplayer/ppedv.Musicplayer.UI.DevConsole/Program.cs
@@ -1,6 +1,9 @@
 ﻿using ppedv.Musicplayer.Logic;
 using ppedv.Musicplayer.Model;
+using ppedv.Musicplayer.Model.Contracts;
 using System;
+using System.Linq;
+using System.Reflection;
 
 namespace ppedv.Musicplayer.UI.DevConsole
 {
@@ -11,16 +14,25 @@ namespace ppedv.Musicplayer.UI.DevConsole
             Console.WriteLine("*** ppedv Musicplayer v0.1 ***");
 
             //dependency injection mit referenz
-            var core = new Core(new Musicplayer.Data.EfCore.EfRepository()); 
+            //var core = new Core(new Musicplayer.Data.EfCore.EfRepository()); 
+
+            //dependency injection ohne Referenz, per reflection
+            var efRepoPath = $@"C:\Users\Fred\source\repos\Architektur_Juni_2021\ppedv.Musicplayer\ppedv.Musicplayer.Data.EfCore\bin\Debug\net5.0\ppedv.Musicplayer.Data.EfCore.dll";
+            var ass = Assembly.LoadFrom(efRepoPath);
+            var typeMitRepo = ass.GetTypes().FirstOrDefault(x => x.GetInterfaces().Contains(typeof(IRepository)));
+            IRepository repo = (IRepository)Activator.CreateInstance(typeMitRepo);
+            var core = new Core(repo);
+
 
             foreach (var a in core.Repository.GetAll<Artist>())
             {
                 Console.WriteLine($"{a.Name} {a.City} {a.Country} {a.BirthDate}");
+                Console.WriteLine($"\t{string.Join(", ", a.Songs.Select(x => x.Title))}");
             }
 
             var aa = core.GetArtistWithMostSongs();
 
-            Console.WriteLine($"Most Songs: {aa.Name}");
+            Console.WriteLine($"Most Songs: {aa?.Name}");
 
 
             Console.WriteLine("Ende");
