4b4dd6819c430db28ca24850f3c796d078c05d0a
André <AndreR@ppedv.de>
2021-07-13 11:02:46 +0200

    EFCore konfiguriert

---
+++ ppedv.Musicplayer/ppedv.Musicplayer.Data.EfCore/EfContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ppedv.Musicplayer.Model;
+using System;
+
+namespace ppedv.Musicplayer.Data.EfCore
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MusicplayerDB_dev;Trusted_Connection=true;");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
