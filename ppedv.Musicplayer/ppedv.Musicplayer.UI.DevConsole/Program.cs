ee60bf11486474b4e8634a733a69cec3115c297e
André <AndreR@ppedv.de>
2021-07-14 11:05:56 +0200

    AutoFac als IOC Container

--- ppedv.Musicplayer/ppedv.Musicplayer.UI.DevConsole/Program.cs
+++ ppedv.Musicplayer/ppedv.Musicplayer.UI.DevConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using ppedv.Musicplayer.Logic;
+﻿using Autofac;
+using ppedv.Musicplayer.Logic;
 using ppedv.Musicplayer.Model;
 using ppedv.Musicplayer.Model.Contracts;
 using System;
@@ -13,17 +14,35 @@ namespace ppedv.Musicplayer.UI.DevConsole
         {
             Console.WriteLine("*** ppedv Musicplayer v0.1 ***");
 
-            //dependency injection mit referenz
+            #region dependency injection mit referenz
             //var core = new Core(new Musicplayer.Data.EfCore.EfRepository()); 
+            #endregion
 
-            //dependency injection ohne Referenz, per reflection
-            var efRepoPath = $@"C:\Users\Fred\source\repos\Architektur_Juni_2021\ppedv.Musicplayer\ppedv.Musicplayer.Data.EfCore\bin\Debug\net5.0\ppedv.Musicplayer.Data.EfCore.dll";
-            var ass = Assembly.LoadFrom(efRepoPath);
-            var typeMitRepo = ass.GetTypes().FirstOrDefault(x => x.GetInterfaces().Contains(typeof(IRepository)));
-            IRepository repo = (IRepository)Activator.CreateInstance(typeMitRepo);
-            var core = new Core(repo);
+            #region dependency injection ohne Referenz auf ppedv.Musicplayer.Data.EfCore, per reflection
+            //var efRepoPath = $@"C:\Users\Fred\source\repos\Architektur_Juni_2021\ppedv.Musicplayer\ppedv.Musicplayer.Data.EfCore\bin\Debug\net5.0\ppedv.Musicplayer.Data.EfCore.dll";
+            //var ass = Assembly.LoadFrom(efRepoPath);
+            //var typeMitRepo = ass.GetTypes().FirstOrDefault(x => x.GetInterfaces().Contains(typeof(IRepository)));
+            //IRepository repo = (IRepository)Activator.CreateInstance(typeMitRepo);
+            //var core = new Core(repo);
+            #endregion
 
-            //todo DI Framework
+            #region AutoFac als DI Framework ohne Referenz auf ppedv.Musicplayer.Data.EfCore
+            //var efRepoPath = $@"C:\Users\Fred\source\repos\Architektur_Juni_2021\ppedv.Musicplayer\ppedv.Musicplayer.Data.EfCore\bin\Debug\net5.0\ppedv.Musicplayer.Data.EfCore.dll";
+            //var ass = Assembly.LoadFrom(efRepoPath);
+            //var builder = new ContainerBuilder();
+            //builder.RegisterAssemblyTypes(ass).Where(t => t.Name.EndsWith("Repository")).AsImplementedInterfaces();
+            //var container = builder.Build();
+
+            //var core = new Core(container.Resolve<IRepository>());
+            #endregion
+
+            #region AutoFac als DI Framework mit Referenz auf ppedv.Musicplayer.Data.EfCore
+            var builder = new ContainerBuilder();
+            builder.RegisterType<Musicplayer.Data.EfCore.EfRepository>().AsImplementedInterfaces();
+            var container = builder.Build();
+
+            var core = new Core(container.Resolve<IRepository>());
+            #endregion
 
 
             foreach (var a in core.Repository.Query<Artist>().Where(x => x.Name.Contains("a")).OrderBy(x => x.BirthDate).ToList())
