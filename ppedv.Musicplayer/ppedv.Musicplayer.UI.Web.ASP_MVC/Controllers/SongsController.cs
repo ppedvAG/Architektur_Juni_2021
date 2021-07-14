401a352fb74de2d7e863831f708849147b317822
André <AndreR@ppedv.de>
2021-07-14 09:38:38 +0200

    ASP MVC

---
+++ ppedv.Musicplayer/ppedv.Musicplayer.UI.Web.ASP_MVC/Controllers/SongsController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ppedv.Musicplayer.Logic;
+using ppedv.Musicplayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ppedv.Musicplayer.UI.Web.ASP_MVC.Controllers
+{
+    public class SongsController : Controller
+    {
+        // GET: SongsController
+        public ActionResult Index()
+        {
+            var core = new Core(new Data.EfCore.EfRepository());
+
+            return View(core.Repository.Query<Song>().ToList());
+        }
+
+        // GET: SongsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SongsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SongsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SongsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SongsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SongsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SongsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
